4ce8d1df45783f5467c31fbe1723f3da4a7a48d0
Holland Ninow <holland.ninow@gmail.com>
2023-07-19 07:51:35 -0700

    add boolean parameter for createBaseHTML() for flexible output - fullHTML for standalone page and partialHTML markup for insertion to an existing HTML doc

--- benfordVisualizer/benfordVisualizer.js
+++ benfordVisualizer/benfordVisualizer.js
@@ -47,7 +47,9 @@ class BenfordVisualizer {
     return chartCodeArray;
   }
 
-  createBaseHTML(companyBenfordObj) {
+  // fullHTML = true if you want HTML markup that works as a standalone page. 
+  // fullHTML = false if you want markup you can insert into another HTML page.
+  createBaseHTML(companyBenfordObj, fullHTML = true) {
     const chartCodeArray = this.bundleChartCode(companyBenfordObj.getStatementBenfordArray());
     const ticker = companyBenfordObj.getTicker() === 'Multiple' ? companyBenfordObj.getSector() : companyBenfordObj.getTicker();
 
@@ -63,6 +65,56 @@ class BenfordVisualizer {
       chartDivArrStr += `<div id="myDiv${i}" style="width:1000px;height:400px;"></div>`;
     }
 
+    const middleHTML = `
+      <style>
+        h1 {
+          font-family: "Open Sans", verdana, arial, sans-serif;
+          font-weight: 400;
+        }
+
+        body {
+          display: flex;
+          flex-direction: column;
+          align-items: center;
+        }
+      </style>
+      <body>
+        <h1>Benford's Law Analysis: ${ticker}</h1>
+        <script src="https://cdn.plot.ly/plotly-2.20.0.min.js" charset="utf-8"></script>
+        ${chartDivArrStr}
+        <script>
+          const xValue = ['1', '2', '3', '4', '5', '6', '7', '8', '9'];
+
+          // Benford frequencies
+          const yBenford = [];
+          for(let i = 1; i < 10; i++) {
+            yBenford.push( Math.log10(1 + (1/i)));
+          }
+          
+          const benford = {
+            x: xValue,
+            y: yBenford,
+            type: 'line',
+            text: yBenford.map(String),
+            textposition: 'auto',
+            name: 'Benford Frequency',
+            hoverinfo: 'none',
+            marker: {
+              color: 'rgba(255,0,0,.5)',
+              line: {
+                color: 'rgb(8,48,107)',
+                width: 1.5
+              }
+            }
+          };
+
+          // Chart JS:
+          ${chartCodeArrStr}      
+
+        </script>
+      </body>
+    `
+
     const baseHTML = `
     <!DOCTYPE html>
     <html lang="en">
@@ -72,56 +124,11 @@ class BenfordVisualizer {
       <meta name="viewport" content="width=device-width, initial-scale=1.0">
       <title>${ticker}</title>
     </head>
-    <style>
-      h1 {
-        font-family: "Open Sans", verdana, arial, sans-serif;
-        font-weight: 400;
-      }
-
-      body {
-        display: flex;
-        flex-direction: column;
-        align-items: center;
-      }
-    </style>
-    <body>
-      <h1>Benford's Law Analysis: ${ticker}</h1>
-      <script src="https://cdn.plot.ly/plotly-2.20.0.min.js" charset="utf-8"></script>
-      ${chartDivArrStr}
-      <script>
-        const xValue = ['1', '2', '3', '4', '5', '6', '7', '8', '9'];
-
-        // Benford frequencies
-        const yBenford = [];
-        for(let i = 1; i < 10; i++) {
-          yBenford.push( Math.log10(1 + (1/i)));
-        }
-        
-        const benford = {
-          x: xValue,
-          y: yBenford,
-          type: 'line',
-          text: yBenford.map(String),
-          textposition: 'auto',
-          name: 'Benford Frequency',
-          hoverinfo: 'none',
-          marker: {
-            color: 'rgba(255,0,0,.5)',
-            line: {
-              color: 'rgb(8,48,107)',
-              width: 1.5
-            }
-          }
-        };
-
-        // Chart JS:
-        ${chartCodeArrStr}      
-
-      </script>
-    </body>
+    ${middleHTML}
     </html>
     `
-    return baseHTML;
+
+    return fullHTML === true ? baseHTML : middleHTML;
   }
 }
 
