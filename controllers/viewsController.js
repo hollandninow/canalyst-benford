ebc3d4f102bdbf52baa2ac853f8e2c65e114c1f3
Holland Ninow <holland.ninow@gmail.com>
2023-07-23 10:39:00 -0700

    add viewsController

--- controllers/viewsController.js
+++ controllers/viewsController.js
@@ -1,1 +1,7 @@
-// TODO:
+const catchAsync = require('../utils/catchAsync');
+
+exports.getAppView = catchAsync(async (req, res, next) => {
+  res.status(200).render('app', {
+    title: 'App',
+  });
+});
